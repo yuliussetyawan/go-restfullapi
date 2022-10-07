055a1c7471a395dee63634fbe766ff33f205d77a
yuliussetyawan <66813917+yuliussetyawan@users.noreply.github.com>
2022-10-07 18:46:59 +0700

    Add files via upload

---
+++ base.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"restfullapi/config"
+	"restfullapi/controllers"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main(){
+	db := config.DBInit()
+	inDB := &controllers.InDB{DB:db}
+	router := gin.Default()
+	router.GET("/person/:id", inDB.GetPerson)
+	router.GET("/persons", inDB.GetPersons)
+	router.POST("/person", inDB.CreatePerson)
+	router.PUT("/person", inDB.UpdatePerson)
+	router.DELETE("/person/:id", inDB.DeletePerson)
+	router.Run(":3000")
+}
